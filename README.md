378574e454805378e4316d0108ad11d75245cdc3
Kyle-Shumway <kshumway@teladochealth.com>
2022-04-06 17:33:03 -0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # AirGarage-Challenge
+
+Both frontend and backend were built using node Node.js v14.17.0
+
+to run either the front-end or back-end, open each respetive folder in a terminal tab and run 
+` npm install `
+to install dependencies and 
+` npm start `
+to start the project
+
+these are the unpaid liscense plates that are output.
+
+XO301
+
+ZQZ984
+
+LFM412
+
+ZLF425
+
+NTA582
+
+E3R456S
---
+++ challenge-backend/src/routes/index.js
@@ -0,0 +1,102 @@
+import https from "https";
+
+const characterVariations = {
+    "4" : "A",
+    "A" : "4",
+    "0" : "O",
+    "O" : "0",
+    "1" : "I",
+    "I" : "1",
+}
+
+async function request(url) {
+    return new Promise((resolve, reject) => {
+        const req = https.get(url, (response) => {
+        let body = [];
+        response.on('data', function(chunk) {
+            body.push(chunk);
+        });
+        // resolve on end
+        response.on('end', function() {
+            try {
+                body = JSON.parse(Buffer.concat(body).toString());
+            } catch(e) {
+                reject(e);
+            }
+            resolve(body);
+        });
+    });
+    req.end();
+});
+}
+async function getPaidPlates() {
+    return request('https://storage.googleapis.com/airgarage/paid.json');
+}
+
+async function getScannedPlates() {
+    return request('https://storage.googleapis.com/airgarage/scans.json');
+}
+
+function isPlatePaid(plate, paid) {
+    return paid.includes(plate);
+}
+
+
+
+function getVariations(plate) {
+    const similarVariations = [];
+    const indexes = [];
+    
+
+    for (let index = 0; index < plate.length; index++) {
+        const character = plate[index];
+        const similar = plate.split("");
+        if (characterVariations[character]) {
+          indexes.push(index);
+          similar[index] = characterVariations[character];
+          similarVariations.push(similar.join(""))
+          if (index > 0) {
+            indexes.forEach((idx) => {
+              if (index !== idx) {
+                similar[idx] = characterVariations[similar[idx]]
+                similarVariations.push(similar.join(""))
+                similar[idx] = plate[idx];
+              }
+            })
+          }
+        }
+        
+    }
+
+    return similarVariations;
+}
+
+function removeHighErrorRateCharacters(plate) {
+    const plateCharacters = plate.split("")
+    return plateCharacters.filter(character => !characterVariations[character]).join();
+}
+
+export default function(app){
+    app.get('/unpaid', async function(req,res) {
+        // get paid plates
+        const paidPlates = await getPaidPlates();
+        // get scanned plates        
+        const scannedPlates = await getScannedPlates();
+        const lookupTable = paidPlates.map((plate, i) => {
+            return {
+                index: i,
+                original: plate,
+                removed: removeHighErrorRateCharacters(plate)
+            }
+        })
+
+        const unpaid = scannedPlates.filter((scannedPlate, i) => {
+            const removed = removeHighErrorRateCharacters(scannedPlate);
+            const found = lookupTable.find((plate) => plate.removed === removed)
+            return !!!found
+        })
+    
+        return res.send(unpaid);
+    });
+  }
+  
