f334490baee5080d1fe39f3a84a3132bcf2db798
Kyle-Shumway <kshumway@teladochealth.com>
2022-04-08 14:39:14 -0700

    string commands together

--- part 2/index.js
+++ part 2/index.js
@@ -5,42 +5,51 @@ async function getLocationData(dataFilePath) {
 }
 
 function locate(state_code, location) {
-    return location.filter((location) => location.address.state === state_code.toUpperCase()).map(location => location.name)
+    return location.filter((location) => location.address.state === state_code.toUpperCase())
 }
 
 function find_price_hourly_lte(price, location) {
-    return location.filter((location) => location.price_hourly <= price).map(location => location.name)
+    return location.filter((location) => location.price_hourly <= price)
 }
 
 function find_price_hourly_gt(price, location) {
-    return location.filter((location) => location.price_hourly > price).map(location => location.name)
+    return location.filter((location) => location.price_hourly > price)
 }
 
 function find_price_hourly_between(price_min, price_max, location) {
-    return location.filter((location) => location.price_hourly > price_min && location.price_hourly < price_max).map(location => location.name)
+    return location.filter((location) => location.price_hourly > price_min && location.price_hourly < price_max)
+}
+
+const utilities = {
+    locate,
+    find_price_hourly_lte,
+    find_price_hourly_gt,
+    find_price_hourly_between
 }
 
 async function exec() {
-    const [first, second, dataFilePath, action, ...params] = process.argv;
+    const [first, second, dataFilePath, ...params] = process.argv;
 
     const location_data = JSON.parse((await getLocationData(dataFilePath)));
-    console.log('params', params)
-    switch (action) {
-        case 'locate':
-            console.log(locate(params[0], location_data));
-            break;
-        case 'find_price_hourly_lte':
-            console.log(find_price_hourly_lte(parseInt(params[0]), location_data));
-            break;
-        case 'find_price_hourly_gt':
-            console.log(find_price_hourly_gt(parseInt(params[0]), location_data));
-            break;
-        case 'find_price_hourly_between':
-            console.log(find_price_hourly_between(parseInt(params[0]), parseInt(params[1]), location_data));
-            break;
-        default:
-            console.log('available commands are: locate, find_price_hourly_lte, find_price_hourly_gt, find_price_hourly_between')
-    }
+    const commands = params.reduce((soFar, current, index) => {
+        if (utilities[current]) {
+            soFar.push({
+                action: utilities[current],
+                params: []
+            })
+        } else {
+            soFar[soFar.length - 1].params.push(current)
+        }
+        return soFar
+    }, [])
+
+    const result = commands.reduce((soFar, command, index) => {
+        const locations = soFar.length ? soFar[index - 1] : location_data;
+        soFar.push(command.action(...command.params, locations));
+        return soFar
+    }, [])
+
+    console.log('Result', result[result.length-1].map(location => location.name))
 }
 
 exec();
